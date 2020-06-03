5920959b0a5faadea448f66f4ca70cb3742cd171
Saurabh <saurabhshukla19011993@gmail.com>
2020-06-03 19:15:18 +0530

    added main-layout.hbs to reduce code rewrite in each shop,add-product and 404 hbs files

--- app.js
+++ app.js
@@ -6,21 +6,28 @@ const expressHbs = require('express-handlebars');
 
 const app = express();
 
-app.engine('hbs', expressHbs({ defaultLayout: '' }));
+app.engine(
+  'hbs',
+  expressHbs({
+    layoutsDir: 'views/layouts',
+    defaultLayout: 'main-layout',
+    extname: 'hbs',
+  })
+);
 app.set('view engine', 'hbs');
 app.set('views', 'views');
 
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).render('404', {pageTitle:'Page Not Found'});
+  res.status(404).render('404', { pageTitle: 'Page Not Found' });
 });
 
 app.listen(3000);
