f51507bc62c163d7328a01fb4838ed21d8bce9b3
Saurabh <saurabhshukla19011993@gmail.com>
2020-06-04 22:33:15 +0530

    added support to add/delete items to cart, edit/delete product from admin-product page

--- routes/admin.js
+++ routes/admin.js
@@ -2,13 +2,24 @@ const express = require('express');
 
 const router = express.Router();
 
-const productsController = require('../controllers/products');
+const adminController = require('../controllers/admin');
 
 // /admin/add-product => GET
-router.get('/add-product', productsController.getAddProduct);
+router.get('/add-product', adminController.getAddProduct);
+
+// /admin/products => GET
+router.get('/products', adminController.getProducts);
 
 // /admin/add-product => POST
-router.post('/add-product', productsController.postAddProduct);
+router.post('/add-product', adminController.postAddProduct);
+
+// /admin/edit-product/:productId => GET
+router.get('/edit-product/:productId', adminController.getEditProduct);
+
+// /admin/edit-product => POST
+router.post('/edit-product', adminController.postEditProduct);
+
+// /admin/delete-product => POST
+router.post('/delete-product', adminController.postDeleteProduct);
 
 module.exports = router;
-
