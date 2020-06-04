dcf5ad786a9582c0880dfeb5d07bb8029d4d510e
Saurabh <saurabhshukla19011993@gmail.com>
2020-06-05 03:34:47 +0530

    added sequelizer to use ORM method to use mysql and added order,cart models

--- controllers/admin.js
+++ controllers/admin.js
@@ -10,16 +10,20 @@ exports.getAddProduct = (req, res, next) => {
 
 exports.postAddProduct = (req, res, next) => {
     const title = req.body.title;
+    const price = req.body.price;
     const imageUrl = req.body.imageUrl;
     const description = req.body.description;
-    const price = req.body.price;
-    const product = new Product(null, title, imageUrl, description, price);
-    product.save()
-        .then(() => {
-            res.redirect('/');
+    req.user.createProduct({
+        title: title,
+        price: price,
+        imageUrl: imageUrl,
+        description: description
+    })
+        .then(result => {
+            console.log('Created Product');
+            res.redirect('/admin/products');
         })
         .catch(err => console.log(err));
-    res.redirect('/');
 };
 
 exports.getEditProduct = (req, res, next) => {
@@ -29,17 +33,21 @@ exports.getEditProduct = (req, res, next) => {
         return res.redirect('/');
     }
     const prodId = req.params.productId;
-    Product.findById(prodId, (product) => {
-        if (!product) {
-            return res.redirect('/');
-        }
-        res.render('admin/edit-product', {
-            pageTitle: 'Edit Product',
-            path: '/admin/edit-product',
-            editing: editMode,
-            product: product,
-        });
-    });
+    req.user.getProducts({ where: { id: prodId } })
+    //Product.findByPk(prodId)
+        .then(products => {
+            const product = products[0];
+            if (!product) {
+                return res.redirect('/');
+            }
+            res.render('admin/edit-product', {
+                pageTitle: 'Edit Product',
+                path: '/admin/edit-product',
+                editing: editMode,
+                product: product,
+            });
+        })
+        .catch(err => console.log(err));
 };
 
 exports.postEditProduct = (req, res, next) => {
@@ -48,30 +56,43 @@ exports.postEditProduct = (req, res, next) => {
     const updatedPrice = req.body.price;
     const updatedImageUrl = req.body.imageUrl;
     const updatedDesc = req.body.description;
-    const updatedProduct = new Product(
-        prodId,
-        updatedTitle,
-        updatedImageUrl,
-        updatedDesc,
-        updatedPrice
-    );
-
-    updatedProduct.save();
-    res.redirect('/admin/products');
+    Product.findByPk(prodId)
+        .then(product => {
+            product.title = updatedTitle;
+            product.price = updatedPrice;
+            product.imageUrl = updatedImageUrl;
+            product.description = updatedDesc;
+            return product.save();
+        })
+        .then(result => {
+            console.log('UPDATED PRODUCT!')
+            res.redirect('/admin/products');
+        })
+        .catch(err => console.log(err));
 };
 
 exports.getProducts = (req, res, next) => {
-    Product.fetchAll((products) => {
-        res.render('admin/products', {
-          prods: products,
-          pageTitle: 'Admin Products',
-          path: '/admin/products',
-        });
-    });
+    req.user.getProducts()
+    // Product.findAll()
+        .then(products => {
+            res.render('admin/products', {
+              prods: products,
+              pageTitle: 'Admin Products',
+              path: '/admin/products',
+            });
+        })
+        .catch(err => console.log(err));
 };
 
 exports.postDeleteProduct = (req, res, next) => {
     const prodId = req.body.productId;
-    Product.deleteById(prodId);
-    res.redirect('/admin/products');
+    Product.findByPk(prodId)
+        .then(product => {
+            return product.destroy();
+        })
+        .then(result => {
+            console.log('DESTROYED PRODUCT!');
+            res.redirect('/admin/products');
+        })
+        .catch(err => console.log(err));
 };
--- util/database.js
+++ util/database.js
@@ -1,10 +1,13 @@
-const mysql = require('mysql2');
+const Sequelize = require('sequelize');
 
-const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'node-shop',
-    password: 'Test@123'
-});
+const sequelize = new Sequelize(
+    'node-shop', 
+    'root', 
+    'Test@123', 
+    {
+        dialect: 'mysql',
+        host: 'localhost',
+    }
+);
 
-module.exports = pool.promise();
+module.exports = sequelize;
