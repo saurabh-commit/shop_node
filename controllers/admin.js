edacb1921ad3fd97c4995fe0a4f2e33e8c1fa73e
Saurabh <saurabhshukla19011993@gmail.com>
2020-06-05 00:09:52 +0530

    shifting data storage on file to storing data in mysql database

--- controllers/admin.js
+++ controllers/admin.js
@@ -14,7 +14,11 @@ exports.postAddProduct = (req, res, next) => {
     const description = req.body.description;
     const price = req.body.price;
     const product = new Product(null, title, imageUrl, description, price);
-    product.save();
+    product.save()
+        .then(() => {
+            res.redirect('/');
+        })
+        .catch(err => console.log(err));
     res.redirect('/');
 };
 
