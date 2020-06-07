2fa957ec7efa28cc7d5075d9b6e35902f661a10a
Saurabh <saurabhshukla19011993@gmail.com>
2020-06-07 18:53:29 +0530

    removed sequelize and added NOSQL database mongoDB to store cart, orders and user data

--- util/database.js
+++ util/database.js
@@ -1,13 +1,29 @@
-const Sequelize = require('sequelize');
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
 
-const sequelize = new Sequelize(
-    'node-shop', 
-    'root', 
-    'Test@123', 
-    {
-        dialect: 'mysql',
-        host: 'localhost',
+let _db;
+
+const mongoConnect = () => {
+    MongoClient.connect(
+        'mongodb+srv://admin93:Test@1234@cluster0-pgrut.mongodb.net/shop?retryWrites=true&w=majority',
+        {useNewUrlParser: true, useUnifiedTopology: true}
+    )
+        .then(client => {
+            console.log('Connected');
+            _db = client.db();
+        })
+        .catch(err => {
+            console.log(err);
+            throw err;
+        });
+}
+
+const getDb = () => {
+    if (_db) {
+        return _db;
     }
-);
+    throw 'No databases found!';
+}
 
-module.exports = sequelize;
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
