12da78dff0e6651861d5678f3d891c9c18399c78
Saurabh <saurabhshukla19011993@gmail.com>
2020-06-09 02:39:24 +0530

    added password resetting and authorization code to only user who created product can change it

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,19 +1,11 @@
+const crypto = require('crypto');
+
 const bcrypt =  require('bcryptjs');
 const sgMail = require('@sendgrid/mail');
 const SENDGRID_API_KEY = 
   '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';
  
 sgMail.setApiKey(SENDGRID_API_KEY);
- 
-const msg = {
-  to: 'saurabhshukl@juniper.net',
-  from: 'saurabhshukla19011993@gmail.com',  //use the email you used to signup to sendgrid
-  subject: 'Sending with Twilio SendGrid is Fun',
-  text: 'and easy to do anywhere, even with Node.js',
-  html: '<strong>and easy to do anywhere, even with Node.js</strong>',
-};
- 
-// sgMail.send(msg);
 
 const User = require('../models/user');
 
@@ -104,7 +96,13 @@ exports.postSignup = (req, res, next) => {
         })
         .then(result => {
           res.redirect('/login');
-          return sgMail.send(msg);
+          return sgMail.send({
+            to: email,
+            from: 'shop@node-complete.com',
+            subject: 'Signup succeeded.',
+            text: 'You successfully signed up!',
+            html: '<strong>You successfully signed up!</strong>',
+          });
         })
         .catch(err => console.log(err));
     })
@@ -117,3 +115,104 @@ exports.postLogout = (req, res, next) => {
     res.redirect('/');
   });
 };
+
+exports.getReset = (req, res, next) => {
+  let message = req.flash('error');
+  if (message.length > 0) {
+    message = message[0];
+  } else {
+    message = null;
+  }
+  res.render('auth/reset', {
+    path: '/reset',
+    pageTitle: 'Reset Password',
+    errorMessage: message
+  });
+};
+
+exports.postReset  = (req, res, next) => {
+  crypto.randomBytes(32, (err, buffer) => {
+    if (err) {
+      console.log(err);
+      return res.redirect('/reset');
+    }
+    const token = buffer.toString('hex');
+    User.findOne({ email: req.body.email })
+      .then(user => {
+        if (!user) {
+          req.flash('error', 'No aaount with that email found.');
+          return res.redirect('/reset');
+        }
+        user.resetToken = token;
+        user.resetTokenExpiration = Date.now() + 3600000;
+        return user.save()
+          .then(result => {
+            res.redirect('/');
+            sgMail.send({
+              to: req.body.email,
+              from: 'shop@node-complete.com',
+              subject: 'Password Reset',
+              html: `
+                <p>You requested a password reset</p>
+                <p>Click this <a href="http://localhost:3000/reset/${token}">link</a> to set a new paasword</p>
+              `
+            });
+          });
+      })
+      .catch(err => console.log(err));
+  });
+};
+
+exports.getNewPassword = (req, res, next) => {
+  const token = req.params.token;
+  User.findOne({ 
+    resetToken: token, 
+    resetTokenExpiration: {
+      $gt: Date.now()
+    }})
+    .then(user => {
+      let message = req.flash('error');
+      if (message.length > 0) {
+        message = message[0];
+      } else {
+        message = null;
+      }
+      res.render('auth/new-password', {
+        path: '/new-password',
+        pageTitle: 'New Password',
+        errorMessage: message,
+        userId: user_id.toString(),
+        passwordToken: token
+      });
+    })
+    .catch(err => console.log(err));
+};
+
+exports.postNewPassword = (req, res, next) => {
+  const newPassword = req.body.password;
+  const userId = req.body.userId;
+  const passwordToken = req.body.passwordToken;
+  let resetUser;
+
+  User.findOne({ 
+    resetToken: passwordToken, 
+    resetTokenExpiration: {
+      $gt: Date.now()
+    },
+    _id: userId
+  })
+    .then(user => {
+      resetUser = user;
+      return bcrypt.hash(newPassword, 12);
+    })
+    .then(hashedPassword => {
+      resetUser.password = hashedPassword;
+      resetUser.resetToken = undefined;
+      resetUser.resetTokenExpiration = undefined;
+      return resetUser.save();
+    })
+    .then(result => {
+      res.redirect('/login');
+    })
+    .catch(err => console.log(err));
+}
