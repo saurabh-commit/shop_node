8d090df08629ebc16b92c722f4dfedf08b98c200
Saurabh <saurabhshukla19011993@gmail.com>
2020-06-10 19:44:22 +0530

    added pagination feature in index and products pages to show only 2 items at once

--- controllers/shop.js
+++ controllers/shop.js
@@ -6,14 +6,31 @@ const PDFDocument = require('pdfkit');
 const Product = require('../models/product');
 const Order = require('../models/order');
 
+const ITEMS_PER_PAGE = 2;
+
 exports.getProducts = (req, res, next) => {
+    const page = +req.query.page || 1;
+    let totalItems;
+  
     Product.find()
+        .countDocuments()
+        .then(numProducts => {
+            totalItems = numProducts;
+            return Product.find()
+                .skip((page - 1) * ITEMS_PER_PAGE)
+                .limit(ITEMS_PER_PAGE);
+        })
         .then(products => {
-            console.log(products);
             res.render('shop/product-list', {
                 prods: products,
-                pageTitle: 'All Products',
-                path: '/products'
+                pageTitle: 'Products',
+                path: '/products',
+                currentPage: page,
+                hasNextPage: ITEMS_PER_PAGE * page < totalItems,
+                hasPreviousPage: page > 1,
+                nextPage: page + 1,
+                previousPage: page - 1,
+                lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE)
             });
         })
         .catch(err => {
@@ -22,15 +39,15 @@ exports.getProducts = (req, res, next) => {
             return next(error);
         });
 };
-
+  
 exports.getProduct = (req, res, next) => {
     const prodId = req.params.productId;
     Product.findById(prodId)
-        .then((product) => {
+        .then(product => {
             res.render('shop/product-detail', {
-                product: product,
-                pageTitle: product.title,
-                path: '/products'
+              product: product,
+              pageTitle: product.title,
+              path: '/products'
             });
         })
         .catch(err => {
@@ -41,12 +58,28 @@ exports.getProduct = (req, res, next) => {
 };
 
 exports.getIndex = (req, res, next) => {
+    const page = +req.query.page || 1;
+    let totalItems;
+  
     Product.find()
+        .countDocuments()
+        .then(numProducts => {
+            totalItems = numProducts;
+            return Product.find()
+                .skip((page - 1) * ITEMS_PER_PAGE)
+                .limit(ITEMS_PER_PAGE);
+        })
         .then(products => {
             res.render('shop/index', {
                 prods: products,
                 pageTitle: 'Shop',
-                path: '/'
+                path: '/',
+                currentPage: page,
+                hasNextPage: ITEMS_PER_PAGE * page < totalItems,
+                hasPreviousPage: page > 1,
+                nextPage: page + 1,
+                previousPage: page - 1,
+                lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE)
             });
         })
         .catch(err => {
@@ -55,6 +88,7 @@ exports.getIndex = (req, res, next) => {
             return next(error);
         });
 };
+  
 
 exports.getCart = (req, res, next) => {
     req.user.populate('cart.items.productId')
